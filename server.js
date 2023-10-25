7845b1691d2c2be82885bbf46b53d82d372f306c
Tanner Norris <jiujitsucarrot@Tanners-MacBook-Air.local>
2023-10-24 22:01:27 -0700

    Modified server.js to continue debugging.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,12 +12,19 @@ app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social_network_db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
 });
 
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+
 // Define Mongoose models 
-const { User, Thought, Reaction } = require('../models');
+const { User, Thought, Reaction } = require(path.join(__dirname, 'models'));
 
 // Define Routes
 const usersRoutes = require('./routes/api/users');
@@ -32,5 +38,7 @@ app.use('/api/reactions', reactionsRoutes);
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
 });
 
