1450fc6bc68f8dba74e6a34e53d52826fa5c100d
Tanner Norris <jiujitsucarrot@Tanners-MacBook-Air.local>
2023-10-31 20:01:55 -0700

    Added sample data to seed.js file. Still debugging routes.

--- utils/seed.js
+++ utils/seed.js
@@ -11,24 +11,97 @@ mongoose.connect('mongodb://localhost/social_network_db', {
 // Define sample user data for seeding
 const usersData = [
     {
-        username: 'user1',
-        email: 'user1@example.com',
+        username: 'MethodMan',
+        email: 'method@wumail.com',
     },
     {
-        username: 'user2',
-        email: 'user2@example.com',
+        username: 'DirtMcGirt',
+        email: 'odb@wumail.com',
     },
+    {
+        username: 'Ghostface',
+        email: 'ghostfacek@wumail.com',
+    },
+    {
+        username: 'Raekwon',
+        email: 'thachef@wumail.com',
+    },
+    {
+        username: 'RZA',
+        email: 'tharizza@wumail.com',
+    },
+    {
+        username: 'TheGenius',
+        email: 'genius@wumail.com',
+    },
+    {
+        username: 'CappaDonna',
+        email: 'cappadonna@wumail.com',
+    },
+    {
+        username: 'Inspectah',
+        email: 'inspectahdeck@wumail.com',
+    },
+    {
+        username: 'UGod',
+        email: 'babyu@wumail.com',
+    },
+    {
+        username: 'MastaK',
+        email: 'mastakillah@wumail.com',
+    },
+    {
+        username: 'RedMan',
+        email: 'redman@wumail.com',
+    },
+
 ];
 
 // Define sample thought data for seeding
 const thoughtsData = [
     {
-        thoughtText: 'This is thought 1.',
-        username: 'user1',
+        thoughtText: 'Better freeze in ya tracks, a Wu-Tang bzzzz killah bees on ya back.',
+        username: 'MethodMan',
     },
     {
-        thoughtText: 'This is thought 2.',
-        username: 'user2',
+        thoughtText: 'Wu-Tang killah bees on the swarm.',
+        username: 'DirtMcGirt',
+    },
+    {
+        thoughtText: 'For cryin out loud, my style is wild so book me. Not long is how long that this rhyme took me.',
+        username: 'Ghostface',
+    },
+    {
+        thoughtText: 'Say peace to cats who rock mad knowledge, knowledgists. Street astrologists, light up the mic yo acknowledge this.',
+        username: 'Raekwon',
+    },
+    {
+        thoughtText: 'Stroll with the holy roll, then attack the globe with the buckus. Style the ruckus.',
+        username: 'RZA',
+    },
+    {
+        thoughtText: 'Unnecessary beef is more cows to breed.',
+        username: 'TheGenius',
+    },
+    {
+        thoughtText: 'Cappa Donna sauna, old school lets go inferno.',
+        username: 'CappaDonna',
+    },
+    {
+        thoughtText: 'The lone ranger, code red. DANGER. Deep in the dark with the art, to rip the charts apart.',
+        username: 'Inspectah',
+    },
+    {
+        thoughtText: 'My hip hop will rock and shock the nation. Like the Emancipation Proclamation.',
+        username: 'UGod',
+    },
+    {
+        thoughtText: 'The flow, changes like a chameleon.',
+        username: 'MastaK',
+    },
+    {
+        thoughtText: 'Doc, Meth back in the flesh. Blood and bones, dont condone spend bank loans on homegrown.',
+        username: 'RedMan',
     },
 ];
 
